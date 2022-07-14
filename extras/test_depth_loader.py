95e98bc278baa290798bc6770ae61663eeb8792e
hardik01shah <hardik01shah@gmail.com>
2022-07-14 15:20:54 +0200

    update to debug keypoint tracking in basalt

--- extras/test_depth_loader.py
+++ extras/test_depth_loader.py
@@ -8,13 +8,18 @@ from PIL import Image
 from scipy.spatial.transform import Rotation
 from torch.utils.data import Dataset
 import pandas as pd
+from tqdm import tqdm
+import cv2
 
 import os
 import yaml
 import shutil
 
 if __name__=="__main__":
-    depth_dir = "../../tumvi_data/test/basalt_keyframe_data/keypoints/"
+    depth_dir = Path("../../tumvi_data/test/basalt_keyframe_data/keypoints/")
+    image_dir = Path("../../tumvi_data/test/mav0/cam0/data/")
+    save_dir = Path("../../tumvi_data/test/debug_depth/")
+
     depth_paths = os.listdir(depth_dir)
     target_image_size = (512, 512)
     depth_map = {}
@@ -22,20 +27,21 @@ if __name__=="__main__":
     times = [tns.replace(".txt", "") for tns in depth_paths]
     
     for tns in times:
-        depth_map[tns] = str(Path(depth_dir) / f"{tns}.txt")
+        depth_map[tns] = str(depth_dir / f"{tns}.txt")
     times.sort()
-    for tns in times:
+    
+    for tns in tqdm(times):
         depth = torch.zeros(1, target_image_size[0], target_image_size[1], dtype=torch.float32)
+        img = cv2.imread(str(image_dir / f"{tns}.png"))
+
         with open(depth_map[tns], "r") as f:
             lines = f.readlines()
+        
         points_sz = int(lines[0])
-        print(tns, points_sz)
         for l in lines[1:]:
             tmp = l.split()
             x, y, i_depth = int(float(tmp[0])), int(float(tmp[1])), float(tmp[2])
             depth[0][y][x] = i_depth
-            # np.savetxt('f.txt', depth.numpy().squeeze())
-            t_np = depth.numpy().squeeze() #convert to Numpy array
-            df = pd.DataFrame(t_np) #convert to a dataframe
-            df.to_csv("testfile",index=False) #save to file
-        break
+            img = cv2.circle(img, (x, y), 4, (0, 0, 255), 1)
+        cv2.imwrite(str(save_dir / f"{tns}.png"), img)
+        # break
