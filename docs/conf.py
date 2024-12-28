a2dfe89605c13c2b0110fecd1d0235f1df6f3b1f
hardik01shah <hardik01shah@gmail.com>
2024-12-28 23:04:38 +0100

    Update conf.py to show source repository edit link

--- docs/conf.py
+++ docs/conf.py
@@ -62,3 +62,11 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "hardik01shah", # Username
+    "github_repo": "DenseReconstructionTools", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
